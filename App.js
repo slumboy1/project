f151cfa9ee1670f76d3e7b5d7a4720e2cddcac87
minefefjwi <wda1174@hanmail.net>
2024-07-05 15:56:39 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return(
+    <div>
+      <button>아침</button> 
+      <button>점심</button> 
+      <button>저녁</button>
+      <h2>자외선 온도</h2>
+    </div>
+  )
+    
+}
+
+export default App;
